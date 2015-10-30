0e1f195d25fd5533119a8b4528b08bf4a8dcc562
Alec Rasmussen <alecrasmussen@users.noreply.github.com>
2015-10-30 19:27:49 -0400

    1.0.3 (error tagging)

--- wax.rb
+++ wax.rb
@@ -4,8 +4,8 @@ class Wax < Formula
   depends_on "python3"
   desc "Wax is a protocol for creating and sharing adaptive audio."
   homepage "http://alecrasmussen.com"
-  url "http://alecrasmussen.com/wax/wax-1.0.2.tar.gz"
-  version "1.0.2"
+  url "http://alecrasmussen.com/wax/wax-1.0.3.tar.gz"
+  version "1.0.3"
   sha256 "5acd1b9f528a7354adcc331e1a9a473a6216580e83653a5649a7222c8a13457c"
 
   def install
