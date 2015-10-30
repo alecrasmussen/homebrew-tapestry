eb369a7ab60561ea2dfa663df2e9da050ed858c4
Alec Rasmussen <alecrasmussen@users.noreply.github.com>
2015-10-30 19:24:50 -0400

    move to 1.0.2
    still need to correct sha256

--- wax.rb
+++ wax.rb
@@ -4,14 +4,9 @@ class Wax < Formula
   depends_on "python3"
   desc "Wax is a protocol for creating and sharing adaptive audio."
   homepage "http://alecrasmussen.com"
-  url "http://alecrasmussen.com/wax/wax-1.0.1.tar.gz"
-  version "1.0.1"
+  url "http://alecrasmussen.com/wax/wax-1.0.2.tar.gz"
+  version "1.0.2"
   sha256 "5acd1b9f528a7354adcc331e1a9a473a6216580e83653a5649a7222c8a13457c"
-
-  resource "pydub" do
-    url "https://github.com/jiaaro/pydub/archive/v0.15.0.tar.gz"
-    sha256 "f0195a20a9eb7110e917105ceb00f4a712abaf070150d12a7dd8c2a84b011afe"	
-  end
 
   def install
   	# TODO: install pydub dependency
