67cbe3ef2a02ba06508c970dfe5848df8c633aef
Alec Rasmussen <alecrasmussen@users.noreply.github.com>
2015-12-13 19:26:52 -0500

    update to 1.0.6

--- brew-test.rb
+++ brew-test.rb
@@ -1,17 +1,14 @@
-class BrewTest < Formula
-  desc ""
-  homepage ""
-  url "http://alecrasmussen.com/wax/brew-test-1.0.4.tar.gz"
-  version "1.0.4"
-  sha256 "4524678551d95ce9a165d3f214ed483500b032190455b5e5e41b7906366aaef9"
-
-  resource "secret" do
-  	url "http://alecrasmussen.com/wax/secret.enc.tar.gz"
-  	sha1 "76996515461f192d312e1b63525a297b10c97243"
-  end
+class Wax < Formula
+  depends_on "ffmpeg"
+  depends_on "sox"
+  depends_on "python3"
+  desc "Wax is a protocol for creating and sharing adaptive audio."
+  homepage "http://alecrasmussen.com"
+  url "http://alecrasmussen.com/wax/wax-1.0.6.tar.gz"
+  version "1.0.6"
+  sha256 "826f23454168fe5fbd193c94d202547d4c97524c662efca66a80f9840244e930"
 
   def install
-  	bin.install "brew-test"
+    bin.install "wax"
   end
-
 end
