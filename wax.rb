1a436f1d0c1ca21f7c9c3c5a0b64402a8f5b61ea
Alec Rasmussen <alecrasmussen@users.noreply.github.com>
2015-12-13 19:54:09 -0500

    update the RIGHT file to 1.0.6

--- wax.rb
+++ wax.rb
@@ -4,9 +4,9 @@ class Wax < Formula
   depends_on "python3"
   desc "Wax is a protocol for creating and sharing adaptive audio."
   homepage "http://alecrasmussen.com"
-  url "http://alecrasmussen.com/wax/wax-1.0.4.tar.gz"
-  version "1.0.4"
-  sha256 "0764c8f3b8a57488a795e67591788fdc3798f8a990718025276689b767db228d"
+  url "http://alecrasmussen.com/wax/wax-1.0.6.tar.gz"
+  version "1.0.6"
+  sha256 "826f23454168fe5fbd193c94d202547d4c97524c662efca66a80f9840244e930"
 
   def install
     bin.install "wax"
