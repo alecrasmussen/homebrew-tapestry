6d2c6d32def1f3b710440e2eca032a54f12cc3fe
Alec Rasmussen <alecrasmussen@users.noreply.github.com>
2017-04-07 11:25:15 -0400

    Update wax.rb

--- wax.rb
+++ wax.rb
@@ -5,7 +5,7 @@ class Wax < Formula
   desc "Wax is a protocol for creating and sharing adaptive audio."
   homepage "http://alecrasmussen.com"
   url "http://alecrasmussen.com/wax/wax-1.0.7.tar.gz"
-  version "1.0.7"
+  version "1.0.8"
   sha256 "c8a6a8eef2e1c8e3b19fb9c8edba87f795ab49680a218b8fff22dd7863b988f0"
 
   def install
