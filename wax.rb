0f25902767c4b4a18c7c727efd233d4a2c311785
Alec Rasmussen <alecrasmussen@users.noreply.github.com>
2017-04-07 12:01:53 -0400

    Update hash

--- wax.rb
+++ wax.rb
@@ -6,7 +6,7 @@ class Wax < Formula
   homepage "http://alecrasmussen.com"
   url "http://alecrasmussen.com/wax/wax-1.0.8.tar.gz"
   version "1.0.8"
-  sha256 "50d47e772e57b802ea7a7740838bd1bc569e42415e1c2f3ff32cbe5f71a1b905"
+  sha256 "45cab476d901a34dcc9b6a5800677b1acf2813deff8ab2f4c167d363a2c58346"
 
   def install
     bin.install "wax"
